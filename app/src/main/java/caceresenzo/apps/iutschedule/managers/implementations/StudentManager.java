f41a835901295dbf5a6d44cd673fb8590c499f85
Caceresenzo <caceresenzo1502@gmail.com>
2020-09-06 15:48:29 +0200

    Restored saving and loading student
    But removed the non implemented feature of supporting more than one student.

--- app/src/main/java/caceresenzo/apps/iutschedule/managers/implementations/StudentManager.java
+++ app/src/main/java/caceresenzo/apps/iutschedule/managers/implementations/StudentManager.java
@@ -1,20 +1,14 @@
 package caceresenzo.apps.iutschedule.managers.implementations;
 
-import android.util.Log;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
 import caceresenzo.apps.iutschedule.activities.IntroActivity;
+import caceresenzo.apps.iutschedule.application.ScheduleApplication;
 import caceresenzo.apps.iutschedule.managers.AbstractManager;
 import caceresenzo.apps.iutschedule.models.Student;
 
 public class StudentManager extends AbstractManager {
-	
-	/* Json Key */
-	public static final String JSON_KEY_STUDENTS = "students";
-	public static final String JSON_KEY_SELECTED_STUDENT_ID = "selected_id";
+
+	/* Shared Preferences Keys */
+	public static final String SHARED_PREF_KEY_STUDENT_ID = "current_student_id";
 	
 	/* Tag */
 	public static final String TAG = StudentManager.class.getSimpleName();
@@ -23,34 +17,27 @@ public class StudentManager extends AbstractManager {
 	private static StudentManager INSTANCE;
 	
 	/* User */
-	private final List<Student> students;
-	private Student selectedStudent;
-	
-	/* Variables */
-	private final File savingFile;
+	private Student student;
 	
 	/* Private Constructor */
 	private StudentManager() {
 		super();
-		
-		this.students = new ArrayList<>();
-		this.savingFile = new File(getDataDirectory(), "students.json");
 	}
 	
 	@Override
 	public void initialize() {
 		super.initialize();
 		
-		loadStudents();
+		loadStudent();
 		
 		if (isStudentSetup()) {
-			selectStudent(selectedStudent);
+			selectStudent(student);
 		}
 	}
 	
 	@Override
 	public void step() {
-		saveStudents();
+		saveStudent();
 	}
 	
 	/**
@@ -69,56 +56,22 @@ public class StudentManager extends AbstractManager {
 	}
 	
 	/** Load previously saved students. */
-	private void loadStudents() {
-		students.clear();
-		
-		/*try {
-			FileUtils.forceFileCreation(savingFile);
-			
-			String fileContent = StringUtils.fromFile(savingFile);
-			
-			if (StringUtils.validate(fileContent)) {
-				JsonObject jsonObject = (JsonObject) new JsonParser().parse(fileContent);
-				
-				JsonArray studentsJsonArray = (JsonArray) jsonObject.get(JSON_KEY_STUDENTS);
-				long selectedStudentId = (long) jsonObject.getOrDefault(JSON_KEY_SELECTED_STUDENT_ID, -1);
-				
-				for (Object item : studentsJsonArray) {
-					Student student = Student.fromJsonObject((JsonObject) item);
-					
-					if (student != null) {
-						students.add(student);
-						
-						if (selectedStudentId == -1 || selectedStudentId == student.getId()) {
-							selectedStudent = student;
-						}
-					}
-				}
-			}
-		} catch (Exception exception) {
-			Log.w(TAG, "Failed to load students.", exception);
-			
-			if (exception instanceof JsonException) {
-				savingFile.delete();
-			}
-		}*/
+	private void loadStudent() {
+		long studentId = ScheduleApplication.get().getSharedPreferences().getLong(SHARED_PREF_KEY_STUDENT_ID, -1);
+
+		if (studentId != -1) {
+			this.student = new Student(studentId);
+		}
 	}
 	
 	/** Save students to a JSON form. */
-	private void saveStudents() {
-		/*try {
-			JsonObject jsonObject = new JsonObject();
-			
-			JsonArray studentJsonArray = new JsonArray();
-			studentJsonArray.addAll(students);
-			
-			jsonObject.put(JSON_KEY_STUDENTS, studentJsonArray);
-			jsonObject.put(JSON_KEY_SELECTED_STUDENT_ID, selectedStudent != null ? selectedStudent.getId() : -1);
-			
-			FileUtils.writeStringToFile(jsonObject.toJsonString(), savingFile);
-		} catch (Exception exception) {
-			Log.w(TAG, "Failed to save students.", exception);
-		}*/
+	private void saveStudent() {
+		if (student != null) {
+			ScheduleApplication.get().getSharedPreferences()
+					.edit()
+					.putLong(SHARED_PREF_KEY_STUDENT_ID, student.getId())
+					.commit();
+		}
 	}
 	
 	/**
@@ -129,11 +82,7 @@ public class StudentManager extends AbstractManager {
 	 *            New {@link Student student}.
 	 */
 	public void selectStudent(Student student) {
-		this.selectedStudent = student;
-		
-		if (!students.contains(student)) {
-			students.add(student);
-		}
+		this.student = student;
 		
 		VirtualCalendarManager.get().invalidateCurrentCalendar();
 		VirtualCalendarManager.get().refreshCalendar();
@@ -141,15 +90,15 @@ public class StudentManager extends AbstractManager {
 	
 	/** @return Selected {@link Student student} that will get his calendar notified. */
 	public Student getSelectedStudent() {
-		return selectedStudent;
+		return student;
 	}
 	
 	/**
-	 * @return Weather or not a {@link Student student} has aready been setup or not.<br>
+	 * @return Weather or not a {@link Student student} has already been setup or not.<br>
 	 *         If not, then the application should start the configuration sequence.
 	 */
 	public boolean isStudentSetup() {
-		return selectedStudent != null;
+		return student != null;
 	}
 	
 	/** @return UserManager's singleton instance. */
--- app/src/main/java/caceresenzo/apps/iutschedule/models/Student.java
+++ app/src/main/java/caceresenzo/apps/iutschedule/models/Student.java
@@ -1,9 +1,6 @@
 package caceresenzo.apps.iutschedule.models;
 
-public class Student /*implements JsonAware*/ {
-	
-	/* Json Key */
-	public static final String JSON_KEY_ID = "id";
+public class Student {
 	
 	/* Variables */
 	private final long id;
@@ -17,53 +14,5 @@ public class Student /*implements JsonAware*/ {
 	public long getId() {
 		return id;
 	}
-	
-	/*@Override
-	public String toJsonString() {
-		JsonObject jsonObject = new JsonObject();
-		
-		jsonObject.put(JSON_KEY_ID, id);
-		
-		return jsonObject.toJsonString();
-	}*/
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
-		return result;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Student other = (Student) obj;
-		if (id != other.id)
-			return false;
-		return true;
-	}
-	
-	/**
-	 * Create a {@link Student student} instance from a {@link JsonObject JSON object}.
-	 * 
-	 * @param jsonObject
-	 *            Source {@link JsonObject JSON object}.
-	 * @return A {@link Student student} instance or <code>null</code> if the <code>id</code> or the <code>name</code> is not valid.
-	 */
-	/*public static Student fromJsonObject(JsonObject jsonObject) {
-		long id = (long) jsonObject.getOrDefault(JSON_KEY_ID, -1);
-		
-		if (id == -1) {
-			return null;
-		}
-		
-		return new Student(id);
-	}*/
 	
 }
